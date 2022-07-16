c28609bc0587b67a2803c4c25928209057e69209
ComputeriFan431 <zaydalba@hotmail.com>
2022-07-16 12:25:25 +0100

    Add project files.

---
+++ AdvancedCalculator.Library/StringOp.h
@@ -0,0 +1,16 @@
+#ifndef STRINGOP_H
+#define STRINGOP_H
+#include <stdlib.h>
+#include <string.h>
+enum BOOL
+{
+	FALSE = 0,
+	TRUE = 1,
+};
+BOOL CompareStr(const char* str1, const char* str2)
+{
+	enum BOOL output = TRUE;
+
+}
+#endif 
+
